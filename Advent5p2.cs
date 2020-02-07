46cfbb6a9acb3026d14813060262ba3e3181b2ee
Ulugbek Aripov <ulugbeks@gmail.com>
2020-02-06 21:30:23 -0500

    first stage of refactoring

--- Advent5p2.cs
+++ Advent5p2.cs
@@ -6,58 +6,136 @@ namespace Advent3
 {
   public class Advent5p2
   {
-    public string getUpdatedIntCode(string intCode, string inp)
+    List<int> IntCode;
+    int nextInstructionStartPoint = 0;
+    public string getUpdatedIntCode(string intCodeFromConsole, string inp)
     {
-      int position = 0;
-      int input = int.Parse(inp);
-      string output = null;
-      List<int> IntCode = intCode.Length > 2
-       ? new List<string>(intCode.Split(",")).Select(int.Parse).ToList()
-       : RawData.dataForAdvent5();
+      int userInput = int.Parse(inp);
+      int output = 0;
+      List<int> instructionParams;
+      IntCode = getIntCode();
+      string nextInstruction;
+      int operationCode;
+      int p1, p2, p3;
 
-      while (position < IntCode.Count)
+      while (nextInstructionStartPoint < IntCode.Count)
       {
-        string abcde = IntCode[position].ToString();
-        abcde = "00000".Substring(abcde.Length) + abcde;
-        int opCode = int.Parse(abcde.Substring(3, 2));
-        if (opCode == 99) break;
-        int qnty = (opCode == 3 || opCode == 4) ? 2
-            : ((opCode == 5 || opCode == 6) ? 3 : Math.Min(4, IntCode.Count - position));
-        var aSet = IntCode.GetRange(position, qnty);
-        position += qnty;
-        if (opCode == 3 && aSet.Count > 1) IntCode[aSet[1]] = input;
-        if (opCode == 4 && aSet.Count > 1 && IntCode.Count > aSet[1])
+        nextInstruction = getNextInstruction();
+        operationCode = int.Parse(nextInstruction.Substring(3, 2));
+        if (operationCode == 99) break;
+
+        int digitsQnty = getNextInstructionDigitsQuantity();
+        
+        instructionParams = IntCode.GetRange(nextInstructionStartPoint, digitsQnty);
+        
+        nextInstructionStartPoint += digitsQnty;
+
+        if (opCodeRequiresInputAtPosition()) IntCode[instructionParams[1]] = userInput;
+        
+        if (opCodeRequiresDisclosingOutput())
         {
-          output = IntCode[aSet[1]].ToString(); Console.WriteLine("output -  " + output);
+          output = IntCode[instructionParams[1]]; 
+          Console.WriteLine("output -  " + output);
         }
-        if (aSet.Count > 2)
+        if (instructionParams.Count > 2)
         {
-          int p1 = abcde.Substring(2, 1) == "1" ? aSet[1] : IntCode[aSet[1]];
-          int p2 = abcde.Substring(1, 1) == "1" ? aSet[2] : IntCode[aSet[2]];
-          if (opCode == 5 && aSet.Count > 2 && p1 > 0) position = p2;
-          if (opCode == 6 && aSet.Count > 2 && p1 == 0) position = p2;
-          if (aSet.Count > 3)
+          p1 = getParameterOneDependingOnMode();
+          p2 = getParameterTwoDependingOnMode();
+          if (opCodeRequiresToJumpIf_P1isNotZero()) nextInstructionStartPoint = p2;
+          if (opCodeRequiresToJumpIf_P1isZero()) nextInstructionStartPoint = p2;
+          if (instructionParams.Count > 3)
           {
-            int p3 = abcde.Substring(0, 1) == "1" ? aSet[3] : IntCode[aSet[3]];
-            if (opCode == 7 && aSet.Count > 3)
+            p3 = instructionParams[3];
+            if (operationCode == 7 && instructionParams.Count > 3)
+              ifP1lessThanP2storeOneOtherwiseZeroAtPositionThree();
+            
+            if (operationCode == 8 && instructionParams.Count > 3)
             {
-              if (p1 < p2) IntCode[aSet[3]] = 1;
-              else IntCode[aSet[3]] = 0;
+              ifP1equalsP2storeOneOtherwiseTwoAtPositionThree();
             }
-            if (opCode == 8 && aSet.Count > 3)
+            if (operationCode < 3 && instructionParams.Count > 3)
             {
-              if (p1 == p2) IntCode[aSet[3]] = 1;
-              else IntCode[aSet[3]] = 0;
-            }
-            if (opCode < 3 && aSet.Count > 3)
-            {
-              if (opCode == 1) IntCode[aSet[3]] = p1 + p2;
-              if (opCode == 2) IntCode[aSet[3]] = p1 * p2;
+              if (opCodeRequiresInputAtP3sumOfP1nP2()) IntCode[p3] = p1 + p2;
+              if (opCodeRequiresInputAtP3productOfP1nP2()) IntCode[p3] = p1 * p2;
             }
           }
         }
       }
-      return output;
+      return output.ToString();
+
+      // local (in function) functions
+      List<int> getIntCode()
+      {
+        return intCodeFromConsole.Length > 2
+        ? intCodeFromConsole.Split(",").Select(int.Parse).ToList()
+        : RawData.dataForAdvent5();
+      }
+
+      string getNextInstruction()
+      {
+          string abcde = IntCode[nextInstructionStartPoint].ToString();
+          return "00000".Substring(abcde.Length) + abcde;
+      }
+
+      int getNextInstructionDigitsQuantity()
+      {
+        return (operationCode == 3 || operationCode == 4) ? 2
+              : ((operationCode == 5 || operationCode == 6) ? 3 : Math.Min(4, IntCode.Count - nextInstructionStartPoint));
+      }
+
+      bool opCodeRequiresInputAtPosition()
+      {
+        return operationCode == 3 && instructionParams.Count > 1;
+      }
+
+      bool opCodeRequiresDisclosingOutput()
+      {
+        return operationCode == 4 && instructionParams.Count > 1 && IntCode.Count > instructionParams[1];
+      }
+      
+      bool opCodeRequiresToJumpIf_P1isNotZero()
+      {
+        return operationCode == 5 && instructionParams.Count > 2 && p1 > 0;
+      }
+
+      bool opCodeRequiresToJumpIf_P1isZero()
+      {
+        return operationCode == 6 && instructionParams.Count > 2 && p1 == 0;
+      }
+
+      bool opCodeRequiresInputAtP3sumOfP1nP2()
+      {
+        return operationCode == 1;
+      }
+
+      bool opCodeRequiresInputAtP3productOfP1nP2()
+      {
+        return operationCode == 2;
+      }
+
+      int getParameterOneDependingOnMode()
+      {
+        return nextInstruction.Substring(2, 1) == "1" ? instructionParams[1] : IntCode[instructionParams[1]];
+      }
+      
+      int getParameterTwoDependingOnMode()
+      {
+        return nextInstruction.Substring(1, 1) == "1" ? instructionParams[2] : IntCode[instructionParams[2]];
+      }
+      
+      void ifP1lessThanP2storeOneOtherwiseZeroAtPositionThree()
+      {
+        if (p1 < p2) IntCode[p3] = 1;
+        else IntCode[p3] = 0;
+      }
+
+      void ifP1equalsP2storeOneOtherwiseTwoAtPositionThree()
+      {
+        if (p1 == p2) IntCode[p3] = 1;
+        else IntCode[p3] = 0;
+      }
+
     }
+
   }
 }
--- Advent5p2.test.cs
+++ Advent5p2.test.cs
@@ -4,55 +4,61 @@ namespace Advent3
 {
     public class Advent5p2Test
     {
-    [Fact]
-    public void NotEight1()
+    [Theory]
+    [InlineData("0", "3,9,8,9,10,9,4,9,99,-1,8", "1")]
+    [InlineData("0", "3,9,8,9,10,9,4,9,99,-1,8", "4")]
+    [InlineData("0", "3,9,8,9,10,9,4,9,99,-1,8", "10")]
+    [InlineData("0", "3,9,8,9,10,9,4,9,99,-1,8", "15")]
+    public void NotEight(string equals, string intCode, string input)
     {
-        Assert.Equal("0", new Advent5p2().getUpdatedIntCode("3,9,8,9,10,9,4,9,99,-1,8", "1"));
-        Assert.Equal("0", new Advent5p2().getUpdatedIntCode("3,9,8,9,10,9,4,9,99,-1,8", "4"));
-        Assert.Equal("0", new Advent5p2().getUpdatedIntCode("3,9,8,9,10,9,4,9,99,-1,8", "10"));
-        Assert.Equal("0", new Advent5p2().getUpdatedIntCode("3,9,8,9,10,9,4,9,99,-1,8", "15"));
+        Assert.Equal(equals, new Advent5p2().getUpdatedIntCode(intCode, input));
     }
     [Fact]
-    public void Eight1()
+    public void Eight()
     {
         Assert.Equal("1", new Advent5p2().getUpdatedIntCode("3,9,8,9,10,9,4,9,99,-1,8", "8"));
     }
-    [Fact]
-    public void LessThanEight()
+    [Theory]
+    [InlineData("1", "3,9,7,9,10,9,4,9,99,-1,8", "1")]
+    [InlineData("1", "3,9,7,9,10,9,4,9,99,-1,8", "4")]
+    public void LessThanEight(string equals, string intCode, string input)
     {
-        Assert.Equal("1", new Advent5p2().getUpdatedIntCode("3,9,7,9,10,9,4,9,99,-1,8", "1"));
-        Assert.Equal("1", new Advent5p2().getUpdatedIntCode("3,9,7,9,10,9,4,9,99,-1,8", "4"));
-
+        Assert.Equal(equals, new Advent5p2().getUpdatedIntCode(intCode, input));
     }
 
-    [Fact]
-    public void EightOrAbove()
+    [Theory]
+    [InlineData("0", "3,9,7,9,10,9,4,9,99,-1,8", "8")]
+    [InlineData("0", "3,9,7,9,10,9,4,9,99,-1,8", "10")]
+    [InlineData("0", "3,9,7,9,10,9,4,9,99,-1,8", "15")]
+    public void EightOrAbove(string equals, string intCode, string input)
     {
-        Assert.Equal("0", new Advent5p2().getUpdatedIntCode("3,9,7,9,10,9,4,9,99,-1,8", "8"));
-        Assert.Equal("0", new Advent5p2().getUpdatedIntCode("3,9,7,9,10,9,4,9,99,-1,8", "10"));
-        Assert.Equal("0", new Advent5p2().getUpdatedIntCode("3,9,7,9,10,9,4,9,99,-1,8", "15"));
+        Assert.Equal(equals, new Advent5p2().getUpdatedIntCode(intCode, input));
     }
 
-    [Fact]
-    public void ZeroToZeroOneForMore()
+    [Theory]
+    [InlineData("0", "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", "0")]
+    [InlineData("1", "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", "1")]
+    [InlineData("0", "3,3,1105,-1,9,1101,0,0,12,4,12,99,1", "0")]
+    [InlineData("1", "3,3,1105,-1,9,1101,0,0,12,4,12,99,1", "1")]
+    public void ZeroToZeroOneForMore(string equals, string intCode, string input)
     {
-        Assert.Equal("0", new Advent5p2().getUpdatedIntCode("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", "0"));
-        Assert.Equal("1", new Advent5p2().getUpdatedIntCode("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", "1"));
-        Assert.Equal("0", new Advent5p2().getUpdatedIntCode("3,3,1105,-1,9,1101,0,0,12,4,12,99,1", "0"));
-        Assert.Equal("1", new Advent5p2().getUpdatedIntCode("3,3,1105,-1,9,1101,0,0,12,4,12,99,1", "1"));
+        Assert.Equal(equals, new Advent5p2().getUpdatedIntCode(intCode, input));
     }
-    [Fact]
-    public void BelowEqualOrAboveEight()
+    [Theory]
+    [InlineData("1000", "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", "8")]
+    [InlineData("1001", "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", "10")]
+
+    public void BelowEqualOrAboveEight(string equals, string intCode, string input)
     {
-        Assert.Equal("1000", new Advent5p2().getUpdatedIntCode("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", "8"));
-        Assert.Equal("1001", new Advent5p2().getUpdatedIntCode("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", "10"));
+        Assert.Equal(equals, new Advent5p2().getUpdatedIntCode(intCode, input));
     }
 
-    [Fact]
-    public void ParOneAndPartTwoMainTests()
+    [Theory]
+    [InlineData("4601506", "", "1")]
+    [InlineData("5525561", "", "5")]
+    public void ParOneAndPartTwoMainTests(string equals, string intCode, string input)
     {
-      Assert.Equal("4601506", new Advent5p2().getUpdatedIntCode("", "1")); // Part 1 test
-      Assert.Equal("5525561", new Advent5p2().getUpdatedIntCode("", "5")); // Part 2 test
+      Assert.Equal(equals, new Advent5p2().getUpdatedIntCode(intCode, input));
     }
 
    }
